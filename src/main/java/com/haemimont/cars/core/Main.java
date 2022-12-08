7c312a69aae72a8154b7644d63c8d5aa7f96bd21
Nikiworald <nikolai.radi@abv.bg>
2022-12-08 17:57:11 +0200

    1

--- src/main/java/com/haemimont/cars/core/Main.java
+++ src/main/java/com/haemimont/cars/core/Main.java
@@ -1,8 +1,11 @@
 package com.haemimont.cars.core;
 
+import com.haemimont.cars.core.model.Driver;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(Driver.Reader("src/main/java/csv/cars.csv",5));
 
     }
 }
