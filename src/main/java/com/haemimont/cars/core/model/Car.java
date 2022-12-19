436ad6e706c50ccd468e21158bcf0f5cf5405426
Nikiworald <nikolai.radi@abv.bg>
2022-12-19 17:46:05 +0200

    1.3.1

--- src/main/java/com/haemimont/cars/core/model/Car.java
+++ src/main/java/com/haemimont/cars/core/model/Car.java
@@ -30,4 +30,8 @@ public class Car {
         return identification;
     }
 
+    public String getId(){
+        return getIdentification().getId();
+    }
+
 }
---
+++ src/main/java/com/haemimont/cars/core/tools/Generator.java
@@ -0,0 +1,12 @@
+package com.haemimont.cars.core.tools;
+
+import com.haemimont.cars.core.model.Car;
+import com.haemimont.cars.core.model.Identification;
+
+public class Generator {
+    public static String vinGenerator(Car car){
+        String vin;
+        Identification identification = car.getIdentification();
+
+    }
+}
