f0658cf79f22dc83d37637027c8e5dee6b8450b3
Nikiworald <nikolai.radi@abv.bg>
2023-06-16 16:19:33 +0300

    1.7

--- src/main/java/com/haemimont/cars/core/servlet/ApiTestServlet.java
+++ src/main/java/com/haemimont/cars/core/servlet/ApiTestServlet.java
@@ -2,21 +2,71 @@ package com.haemimont.cars.core.servlet;
 
 import com.haemimont.cars.core.tools.ApiRequest;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.checkerframework.common.reflection.qual.GetMethod;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
+@WebServlet("/ApiTest")
 public class ApiTestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if(ApiRequest.validate(req)){//todo do the api request and get the jwttoken and test the thing
-        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        if (ApiRequest.validate(req)) {//todo do the api request and get the jwttoken and test the thing
+            String name = req.getParameter("name");
+            String password = req.getParameter("password");
+            String email = req.getParameter("email");
+            String userTypes = req.getParameter("userType");
+            String[] userTypesSplit = userTypes.trim().split(",");
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("username", name.trim());
+            jsonObject.put("email", email.trim());
+            jsonObject.put("password", password.trim());
+            jsonObject.put("role", userTypesSplit);
+            URL url = new URL("http://192.168.250.206:8080/api/auth/signup");
+            System.out.println(postRequest(url, jsonObject));
+
+        } else {
+            System.out.println("error");
+        }
+    }
+
+    public String postRequest(URL url, JSONObject jsonObject) {
+        String response = null;
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        try {
+            System.out.println(jsonObject.toString());
+            StringEntity requestEntity = new StringEntity(
+                    jsonObject.toString(),
+                    ContentType.APPLICATION_JSON);
+            System.out.println(requestEntity);
+            HttpPost postRequest = new HttpPost(url.toString());
+            postRequest.setEntity(requestEntity);
+            postRequest.setHeader("Content-type", "application/json");
+            System.out.println(postRequest);
+            HttpResponse rawResponse = httpClient.execute(postRequest);
+            System.out.println(rawResponse);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return response;
     }
 }
--- src/main/java/com/haemimont/cars/core/tools/ApiRequest.java
+++ src/main/java/com/haemimont/cars/core/tools/ApiRequest.java
@@ -4,10 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ApiRequest {
     public static boolean validate(HttpServletRequest request){
         boolean check = false;
-       String name= (String) request.getAttribute("name");
-        String password= (String) request.getAttribute("password");
-        String email= (String) request.getAttribute("email");
-        String userType= (String) request.getAttribute("userType");
+       String name= (String) request.getParameter("name");
+        String password= (String) request.getParameter("password");
+        String email= (String) request.getParameter("email");
+        String userType= (String) request.getParameter("userType");
 
        if(name!=null&&password!=null&&email!=null&&userType!=null) {
            if(name.trim().length()>0&&password.trim().length()>0&&email.trim().length()>0&&userType.trim().length()>0){
