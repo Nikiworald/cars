9f89199eec42e4595d2e23d00307834af9dcfd75
Nikiworald <nikolai.radi@abv.bg>
2023-06-30 12:15:20 +0300

    1.8.3

--- src/main/java/com/haemimont/cars/core/apitest/callables/AuthTestCallable.java
+++ src/main/java/com/haemimont/cars/core/apitest/callables/AuthTestCallable.java
@@ -9,20 +9,20 @@ import java.util.concurrent.Callable;
 public class AuthTestCallable implements Callable<ApiResult> {
 
     private final String URL;
-    private final String JWTTOKEN;
+    private final String JWT_TOKEN;
     private final String URL_NAME_IN_CONFIG;
     private final String NAME_OF_THE_TEST;
 
     public AuthTestCallable(String url, String JWTToken, String urlNameInConfig, String testName) {
         this.URL = url;
-        this.JWTTOKEN = JWTToken;
+        this.JWT_TOKEN = JWTToken;
         this.URL_NAME_IN_CONFIG = urlNameInConfig;
         this.NAME_OF_THE_TEST = testName;
     }
 
     @Override
     public ApiResult call() throws Exception {
-        ApiResult allTestApiResult = ApiOperations.authorizationTest(URL + Config.getPropertyByName(URL_NAME_IN_CONFIG), JWTTOKEN);//no token needed for public access
+        ApiResult allTestApiResult = ApiOperations.authorizationTest(URL + Config.getPropertyByName(URL_NAME_IN_CONFIG), JWT_TOKEN);//no token needed for public access
         allTestApiResult.setName(NAME_OF_THE_TEST);
         return allTestApiResult;
     }
